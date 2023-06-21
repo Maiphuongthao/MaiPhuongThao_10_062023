bd2a41ef5da95d58610e4751d2f6c4f2adfe2410
Thao <thao.maiphuong@outlook.fr>
2023-06-21 20:52:54 +0200

    add apps to settings

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'rest_framework',
     'rest_framework_simplejwt',
+    'users',
+    'projetcs',
+    'issues',
+    'comments',
 ]
 
 MIDDLEWARE = [
