2935126e390b349b389978e6011b3a2103907de0
Thao <thao.maiphuong@outlook.fr>
2023-06-21 20:49:24 +0200

    add apps

--- SoftDesk/SoftDesk/urls.py
+++ SoftDesk/SoftDesk/urls.py
@@ -20,8 +20,11 @@ from django.urls import path, include
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+router = routers.SimpleRouter()
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='obtain_tokens'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('api/', include(router.urls)),
 ]
